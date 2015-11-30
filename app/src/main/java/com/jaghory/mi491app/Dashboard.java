06bdd21b0a323ba85faa070d5c85d7ce57029ab6
Zachary Jaghory <zj@zach-jaghory.com>
2015-11-30 11:29:21 -0500

    Basic conversations appear

--- app/src/main/java/com/jaghory/mi491app/Dashboard.java
+++ app/src/main/java/com/jaghory/mi491app/Dashboard.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,11 +17,15 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.firebase.ui.FirebaseRecyclerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
+    private RecyclerView conversationsRecyclerView;
+    private RecyclerView.Adapter conversationsAdapter;
+    private RecyclerView.LayoutManager conversationsLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,30 +35,33 @@ public class Dashboard extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // use a linear layout manager
+        conversationsLayoutManager = new LinearLayoutManager(getApplicationContext());
+        conversationsRecyclerView = (RecyclerView) findViewById(R.id.conversationsRecView);
+        conversationsRecyclerView.setLayoutManager(conversationsLayoutManager);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        Fragment cif = new ConversationsInboxFragment();
-        fragmentTransaction.add(R.id.fragment_inbox, cif);
-        fragmentTransaction.commit();
-        fragmentManager.executePendingTransactions();
+        Firebase mFireRef = new Firebase("https://mi491app.firebaseio.com/conversations");
+        conversationsRecyclerView.setHasFixedSize(true);
+        
+
+        FirebaseRecyclerAdapter mAdapter = new FirebaseRecyclerAdapter<ConversationOverview,ConversationViewHolder>(ConversationOverview.class, android.R.layout.two_line_list_item, ConversationViewHolder.class, mFireRef) {
+            //@Override
+            public void populateViewHolder(ConversationViewHolder chatMessageViewHolder, ConversationOverview chatMessage) {
+                chatMessageViewHolder.nameText.setText(chatMessage.getcTitle());
+                chatMessageViewHolder.messageText.setText(chatMessage.getcSummary());
+                System.out.println("HELLLOOOOO "+chatMessage.getcTitle());
+            }
+        };
+        conversationsRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Snackbar.make(view, "Hello", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();*/
+                Snackbar.make(view, "Hello", Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
 
-
-        List<Fragment> fragments = getSupportFragmentManager().getFragments();
-        for(Fragment fragment : fragments){
-            if(fragment != null && fragment.isVisible())
-                System.out.println(fragment.toString());
-                Snackbar.make(view,String.valueOf(fragment.isVisible()),Snackbar.LENGTH_SHORT).setAction("Action",null).show();
-        }
-            }
-        });
+            }});
 
 
 
@@ -79,6 +87,16 @@ public class Dashboard extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private static class ConversationViewHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView nameText;
+
+        public ConversationViewHolder(View itemView) {
+            super(itemView);
+            nameText = (TextView)itemView.findViewById(android.R.id.text1);
+            messageText = (TextView) itemView.findViewById(android.R.id.text2);
+        }
     }
 }
 
