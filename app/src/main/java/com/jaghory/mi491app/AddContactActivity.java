8149696b7a9e1d53a1ddee78c07d342a7b52c712
Zachary Jaghory <zj@zach-jaghory.com>
2015-12-06 19:39:25 -0500

    Contacts search works!

--- app/src/main/java/com/jaghory/mi491app/AddContactActivity.java
+++ app/src/main/java/com/jaghory/mi491app/AddContactActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.client.DataSnapshot;
@@ -13,6 +15,8 @@ import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
+
+import org.w3c.dom.Text;
 
 public class AddContactActivity extends AppCompatActivity {
 
@@ -25,6 +29,14 @@ public class AddContactActivity extends AppCompatActivity {
         final Button searchButton = (Button) findViewById(R.id.searchButton);
         final EditText searchString = (EditText) findViewById(R.id.editText_phone_search);
         final Firebase tFireRef = new Firebase("https://mi491app.firebaseio.com");
+
+        final Button acceptBtn = (Button) findViewById(R.id.result_accept);
+        final TextView usrName = (TextView) findViewById(R.id.result_username);
+        final TextView usrEmail = (TextView) findViewById(R.id.result_email);
+        final LinearLayout sResults = (LinearLayout) findViewById(R.id.searchResults);
+
+
+
         searchButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -36,6 +48,24 @@ public class AddContactActivity extends AppCompatActivity {
                         if(dataSnapshot.exists()){
                             Toast t=  Toast.makeText(getApplicationContext(), dataSnapshot.getValue().toString(), Toast.LENGTH_SHORT);
                             t.show();
+                            tFireRef.child("users").child(dataSnapshot.getValue().toString()).addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+
+                                    //usrName.setText(dataSnapshot.getValue().toString());
+                                    usrName.setText(dataSnapshot.child("displayName").getValue().toString());
+                                    usrEmail.setText(dataSnapshot.child("username").getValue().toString());
+                                    sResults.setVisibility(View.VISIBLE);
+
+
+                                }
+
+                                @Override
+                                public void onCancelled(FirebaseError firebaseError) {
+
+                                }
+                            });
+
                         }
                         else{
                             Toast t=  Toast.makeText(getApplicationContext(), "No results", Toast.LENGTH_SHORT);
